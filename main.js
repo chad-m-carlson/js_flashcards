8f872802b1c444623989a70892fbce5c61896e68
Chad Carlson <chad.m.carlson@gmail.com>
2019-06-02 10:03:42 -0600

    added form to add new flash card, delete button not quite working

--- main.js
+++ main.js
@@ -1,37 +1,73 @@
 $(document).ready( function() {
-  const cardInfo = (title, description, [...details], snippet) => {
-    return({title, description, details: [...details], snippet, });
+  const cardInfo = (title, description, details, snippet) => {
+    return({title, description, details: details, snippet, });
   }
 
-  let variablesCard = cardInfo("Variables", "'Let', 'Const', or 'Var'?", ['Let', 'Const', 'var'], "var = x" )
-  let objectsCard = cardInfo("Object Initializer", "Easy to add information to an object", ['Name variables the same as keys to avoid repetition', 'Create objects on the fly'])
-  let someOtherCard = cardInfo("blah bah", "blah Blah", ['asdf', 'asdf'], 'asldfkj')
+  let variablesCard = cardInfo("Variables", "'Let', 'Const', or 'Var'?", 'Let, Const ,var', "var = x" )
+  let objectsCard = cardInfo("Object Initializer", "Easy to add information to an object", 'Name variables the same as keys to avoid repetition ,Create objects on the fly')
+  let someOtherCard = cardInfo("blah bah", "blah Blah", 'asdf asdf', 'asldfkj')
 
   let allCards = [variablesCard, objectsCard, someOtherCard]
 
+  $('#submit').on('click', () => {
+    let title = document.getElementById('title');
+    let description = document.getElementById('description');
+    let details = document.getElementById('details');
+    let code_snippet = document.getElementById('code_snippet');
+    let x  = cardInfo(title.value, description.value, details.value, code_snippet.value);
+    allCards.push(x);
+    debugger
+    $('#container').empty();
+    displayCards();
+  })
 
-  $(document).on('click', '.card_container', function(){
-    var _this = this;
-    console.log(_this.id)
-
+  $(document).on('click', '#flip', function() {
+    var _this = this.parentElement;
     $(`#${_this.id} > .${_this.childNodes[0].classList.value}`).children().toggleClass('hidden');
     $(`#${_this.id} > .${_this.childNodes[1].classList.value}`).children().toggleClass('hidden');
   });
 
-  for(i in allCards) {
-  $('#container').append(`<div class="card_container"  id="card${i}"  style="width: 300px; height: 300px; border: 1px solid black"><div class="front_side"></div><div class="back_side"></div></div`)  
-  }
+  $(document).on('click', '#edit', function() {
 
-  for(i in allCards){
-    $(`#card${i} > .front_side`).append(
-    `<h2 class="front_side">${allCards[i].title}</h2>`,
-    `<h3 class="front_side">${allCards[i].description}</h3>`,)
-  }
+  })
 
-  for(i in allCards){
-    for(index in allCards){
-      $(`#card${i} > .back_side`).append(
-        `<h3 class="back_side hidden">${allCards[i].details[index]}</h3>`)
-      }
+  $(document).on('click', '#delete', function() {
+    function remove(array, element) {
+      debugger
+      const index = array.indexOf(element);
+      array.splice(index, 1);
+      debugger
+    }
+// THIS ISN"T REMOVING ITEM FROM ARRAY ALLCARDS
+    remove(allCards, this.parentElement.childNodes[0].childNodes[0].innerText);
+    $(this.parentElement).remove();
+  })
+
+  const displayCards = () => {
+    
+  
+    for(i in allCards) {
+    $('#container').append(`<div class="card_container ui card"  id="card${i}"  ><div class="front_side"></div><div class="back_side"></div></div`)  ;
+    $(`#card${i}`).append(
+      `<button id="flip">Flip Card</button>`);
+    $(`#card${i}`).append(
+      `<button id="edit">Edit</button>`);
+    $(`#card${i}`).append(
+      `<button id="delete">Delete</button>`);
+    }
+
+    for(i in allCards){
+      $(`#card${i} > .front_side`).append(
+      `<h2 class="front_side">${allCards[i].title}</h2>`,
+      `<h3 class="front_side">${allCards[i].description}</h3>`,);
+    }
+
+    for(i in allCards){
+        $(`#card${i} > .back_side`).append(
+          `<h3 class="back_side hidden">${allCards[i].details}</h3>`,
+          );
+    }
   }
+displayCards()
+
 });
